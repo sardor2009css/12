48da9819e222b0f4e2a7921cebec16dfb7c0f285
sardor2009css <sardortoshtemirov621@gmail.com>
2025-09-17 21:06:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let inputNumber = document.querySelector("#inputNumber");
+let sendBtn = document.getElementById("sendBtn");
+let minus = document.getElementById("minus");
+let span = document.getElementById("span");
+let plusBtn = document.getElementById("plusBtn");
+let inputColor = document.getElementById("inputColor");
+
+let body = document.body;
+let counterBox = document.getElementById("counterBox");
+let counterEl = document.getElementById("span"); 
+
+
+sendBtn.addEventListener("click", ()=> {
+    span.textContent = inputNumber.value;
+})
+
+
+plusBtn.addEventListener('click', ()=>{
+    span.textContent = Number(span.textContent) + 1;
+})
+
+minus.addEventListener('click', ()=>{
+    span.textContent = Number(span.textContent) - 1;
+})
+
+
+inputColor.addEventListener('input', ()=> {
+    span.style.color = inputColor.value;
+})
+
+
+body.style.backgroundColor = "green";
+body.style.color = "black"; 
+body.style.fontFamily = "Arial";
+body.style.height = "155vh";
+body.style.display = "flex";
+body.style.flexDirection = "column";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.gap = "42px";
+
